1eb3f29d498cc56633b21f422b78578ebe19d1bf
nands <ferdi@nand.co.za>
2012-10-20 22:42:25 +0200

    Added a code header to index.php
    Also changed a few things in config.ini.

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
+/**
+ * @author		Ferdi Schmidt <ferdi@nand.co.za>
+ * @copyright	Deviant, 2012. 
+ */
 
 use Deviant\Library\Autoloader,
 	Deviant\Library\Config,
 	Deviant\Library\Database,
-    Deviant\Library\Route,
-    Deviant\Library\Router,
-    Deviant\Library\Dispatcher,
-    Deviant\Library\Request,
-    Deviant\Library\Response,
+	Deviant\Library\Route,
+	Deviant\Library\Router,
+	Deviant\Library\Dispatcher,
+	Deviant\Library\Request,
+	Deviant\Library\Response,
 	Deviant\Library\Module;
 
 require_once('lib/autoloader.php');
@@ -18,10 +22,10 @@ $request	= new Request();
 $response	= new Response;
 $dispatcher = new Dispatcher;
 
-$routes		= array();
-$routes[]	= new Route('index', 'Deviant\Module\Index');
-$routes[]	= new Route('test', 'Deviant\Module\Test');
-$routes[]	= new Route('error', 'Deviant\Module\Error');
+$routes = array();
+$routes[] = new Route('index', 'Deviant\Module\Index');
+$routes[] = new Route('test', 'Deviant\Module\Test');
+$routes[] = new Route('error', 'Deviant\Module\Error');
 
 $router = new Router($routes);
 
